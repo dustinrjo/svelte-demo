583810b322d6574294a9101b357f33b2dfb254ac
Dustin Johnson <dustinrjo@gmail.com>
2025-08-27 13:14:58 -0500

    half baked start

---
+++ src/lib/UserInput.svelte
@@ -0,0 +1,34 @@
+<script>
+	let inputValue = '';
+
+	export { inputValue };
+</script>
+
+<div>
+	<label for="user-input">Enter text:</label>
+	<input
+		id="user-input"
+		type="text"
+		bind:value={inputValue}
+		placeholder="Type something here..."
+	/>
+</div>
+
+<style>
+	div {
+		margin: 1rem 0;
+	}
+	
+	label {
+		display: block;
+		margin-bottom: 0.5rem;
+		font-weight: bold;
+	}
+	
+	input {
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		width: 300px;
+	}
+</style>
